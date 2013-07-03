2e22ec88632d31a40a143adad825b588f0ef8db6
Thomas <twatson52@mac.com>
2013-07-03 13:00:54 -0500

    add proxy

--- src/tpw_rules/crappymod/CrappyMod.scala
+++ src/tpw_rules/crappymod/CrappyMod.scala
@@ -4,11 +4,25 @@ import cpw.mods.fml.common.{ Mod, SidedProxy, event }
 import cpw.mods.fml.common.Mod._
 import cpw.mods.fml.common.network.NetworkMod
 
+class CommonProxy {
+  def getSide(): String = "Server"
+}
+
+class ClientProxy extends CommonProxy {
+  override def getSide(): String = "Client"
+}
+
 @Mod(modLanguage="scala", modid="crappymod", name="Crappy Mod", version="1")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 object CrappyMod {
+  @SidedProxy(
+    clientSide="tpw_rules.crappymod.ClientProxy",
+    serverSide="tpw_rules.crappymod.CommonProxy")
+  var proxy: CommonProxy = null
+
   @Mod.EventHandler
   def init(e: event.FMLInitializationEvent) = {
     println("Initialized!")
+    println(proxy.getSide)
   }
 }
